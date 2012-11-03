c03bd31dd84b2e66d4f0f212934e1c8b2b887890
jk <jk@redpai.no>
2012-11-03 23:05:03 +0100

    Post test and models

--- posts/tests.py
+++ posts/tests.py
@@ -1,16 +1,22 @@
 """
 This file demonstrates writing tests using the unittest module. These will pass
 when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from posts.models import Post
+
+def set_up():
+    post = Post(heading='Apply for membership',
+                body="It's worth it"
+                )
+    post.save()
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class PostTest(TestCase):
+    def setUp(self):
+        set_up()
+
+    def test_post_created(self):
+        p = Post.objects.all()[0]
+        self.assertTrue('Apply' in p.heading)
