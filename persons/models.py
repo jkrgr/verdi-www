7819d7e6b6b1ec9e76066a9a267450843512c645
jk <jk@redpai.no>
2012-11-03 22:13:51 +0100

    Tests and models for persons

--- persons/models.py
+++ persons/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from  django.contrib.auth.admin import User
 
-# Create your models here.
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+    position = models.CharField(max_length=255)
+
--- persons/tests.py
+++ persons/tests.py
@@ -2,15 +2,27 @@
 This file demonstrates writing tests using the unittest module. These will pass
 when you run "manage.py test".
 
-Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from persons.models import User
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class PersonTest(TestCase):
+    def setUp(self):
+        """ Create some data to test against
         """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+        p = User()
+        p.first_name = "Joachim"
+        p.last_name = "Kruger"
+        p.email = "jk@redpai.no"
+        p.username = p.email
+        p.position = 'Analyst'
+        p.save()
+
+
+    def test_user(self):
+        pers = User.objects.all()[0]
+        self.assertEqual(pers.first_name, 'Joachim')
+        self.assertEqual(pers.username, 'jk@redpai.no')
+
